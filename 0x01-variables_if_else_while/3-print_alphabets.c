cc0390762f7d437389bc4ae4e90edfd10d50e2d6
Iseoluwadoyin <iseoluwadoyinemmanuel@gmail.com>
2022-07-04 10:37:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: loop to print alphabets in lowercase using  putchar()
+ * Return: 0 Always success
+ */
+int main(void)
+{
+char lower_case;
+char upper_case;
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+{
+putchar(lower_case);
+}
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
+}
+putchar('\n');
+return (0);
+}
