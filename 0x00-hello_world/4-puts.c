4a34ccc6fb007e71f4213d6317eaeef0c4ba7cdd
Iseoluwadoyin <iseoluwadoyinemmanuel@gmail.com>
2022-07-04 00:33:15 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
-/
- * main - Entry point
- *
- *  * Return: Always 0 (sucess)
- *
- *   */
 
- int main(void)
+/** 
+ *
+ *  * main - entry point
+ *
+ *  *
+ *
+ *   * Return: 0 after printing the function
+ *
+ *    */
+
+int main(void)
 
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
+
 }
+
+
