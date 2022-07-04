026797787eec8d61844a55d85a3bee7b8eceddf5
Iseoluwadoyin <iseoluwadoyinemmanuel@gmail.com>
2022-07-04 11:28:10 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 for success
+ */
+int main(void)
+{
+int numbers;
+for (numbers = 48; numbers <= 57; numbers++)
+putchar(numbers);
+putchar('\n');
+return (0);
+}
